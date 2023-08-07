90a1fb8257cf9f95c2a22e7df9f748b0583507ec
maxthestranger <thestrangermax@gmail.com>
2023-08-07 19:17:35 +0300

    fix dashboard error

--- resources/js/Components/Sidebar.jsx
+++ resources/js/Components/Sidebar.jsx
@@ -9,7 +9,9 @@ export default function Sidebar({user, showingNavigationDropdown, setShowingNavi
                 ? 'candidate menu'
                 : role === 'recruiter'
                     ? 'recruiter menu'
-                    : 'dei officer menu';
+                    : role === 'academia'
+                        ? 'academia menu'
+                        : 'dei officer menu';
     return (
         <aside className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static lg:translate-x-0 ${
             showingNavigationDropdown ? 'translate-x-0' : '-translate-x-full'
